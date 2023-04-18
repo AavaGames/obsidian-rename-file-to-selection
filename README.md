b2581d786f3d5f95c6131f819bc641c8af6979f5
Philip F <theaavagames@gmail.com>
2023-04-18 12:23:15 -0300

    Cleaned up main, updated Readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # obsidian-rename-to-selection
-Rename your file or first header to current text selection
+Rename your file to the current text selection.
 
+## Commands
+### Rename Title to Selection
+Uses the current selection / highlighted text in the active document to rename the file after sanitizing it.
 
-#
+##
 Uses [sanitize-filename-ts](https://github.com/hfour/sanitize-filename-ts) to sanitize selection for filenames.
--- main.ts
+++ main.ts
@@ -1,26 +1,8 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import { sanitize } from "sanitize-filename-ts";
 
-// Remember to rename these classes and interfaces!
-
-interface RenameToSelectionPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: RenameToSelectionPluginSettings = {
-	mySetting: 'default'
-}
-
 export default class RenameToSelectionPlugin extends Plugin {
-	settings: RenameToSelectionPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
-
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('Updated');
-		});
-
 		this.addCommand({
 			id: "rename-title-to-selection",
 			name: "Rename Title To Selection",
@@ -43,81 +25,5 @@ export default class RenameToSelectionPlugin extends Plugin {
 				}
 			},
 		});
-
-		this.addCommand({
-			id: "rename-header-to-selection",
-			name: "Rename Header To Selection",
-			editorCallback: (editor: Editor) => {
-				const selection = editor.getSelection();
-				const sanitizedSelection = sanitize(selection)
-				
-				const file = app.workspace.getActiveFile();
-				if (file != null) {
-					const fileContent = app.vault.read(file);
-
-
-
-					//editor.replaceRange()
-				}
-			},
-		});
-
-	}
-
-	async onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class RenameToSelectionModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class RenameToSelectionSettingTab extends PluginSettingTab {
-	plugin: RenameToSelectionPlugin;
-
-	constructor(app: App, plugin: RenameToSelectionPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
